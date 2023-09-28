ece9ad87d8f43e9605881ed0dd356bafe4f1e8be
Leon Menzies <leon.menzies@hotmail.com>
2023-09-27 21:48:52 -0700

    added absolute paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin", "module:react-native-dotenv"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      "module:react-native-dotenv",
+      "module-resolver",
+      {
+        alias: {
+          assets: "./src/assets",
+          components: "./src/components",
+          pages: "./src/pages",
+          recoil: "./src/recoil",
+          styles: "./src/styles",
+          types: "./src/types",
+          utils: "./src/utils",
+        },
+      },
+    ],
   };
 };
