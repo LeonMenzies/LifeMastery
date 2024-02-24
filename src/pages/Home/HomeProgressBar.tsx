7d23c2f9dd9e54789f794813fca5e107a486a722
Leon Menzies <leon.menzies@hotmail.com>
2024-02-24 15:53:28 -0800

    animated progress bar

--- src/pages/Home/HomeProgressBar.tsx
+++ src/pages/Home/HomeProgressBar.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { StyleSheet, View, DimensionValue } from "react-native";
+import { FC, useEffect, useRef } from "react";
+import { StyleSheet, View, Animated } from "react-native";
 import { useRecoilValue } from "recoil";
 
 import { themeAtom } from "~recoil/themeAtom";
@@ -11,23 +11,40 @@ type HomeProgressBarT = {
 
 export const HomeProgressBar: FC<HomeProgressBarT> = ({ percent }) => {
   const colors = useRecoilValue(themeAtom);
-  const styles = styling(colors, `${percent}%`);
+  const styles = styling(colors);
+
+  // Create an animated value
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  // Update the animated value when the percent prop changes
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: percent,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }, [percent]);
+
+  // Calculate the width as a percentage
+  const width = animatedValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+  });
 
   return (
     <View style={styles.container}>
-      <View style={styles.percent} />
+      <Animated.View style={[styles.percent, { width }]} />
     </View>
   );
 };
 
-const styling = (colors: ThemeT, percent: DimensionValue) =>
+const styling = (colors: ThemeT) =>
   StyleSheet.create({
     container: {
       height: 6,
       width: "100%",
     },
     percent: {
-      width: percent,
       height: "100%",
       backgroundColor: colors.secondary,
     },
